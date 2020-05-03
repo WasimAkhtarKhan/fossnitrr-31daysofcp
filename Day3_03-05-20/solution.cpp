a55013bd8864a9e22d9377281f386e23ada910aa
wasim0315 <50516723+wasim0315@users.noreply.github.com>
2020-05-03 21:45:21 +0530

    Add files via upload

---
+++ Day3_03-05-20/solution.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include<map>
+
+using namespace std;
+
+// Complete the matchingStrings function below.
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+long unsigned int i,j,c=0;
+vector<int> arr(queries.size());
+for(i=0;i<queries.size();i++)
+{
+    c=0;
+    for(j=0;j<strings.size();j++)
+    {
+        if(queries[i]==strings[j])
+        c++;
+    }
+    arr[i]=c;
+}
+return arr;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int strings_count;
+    cin >> strings_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> strings(strings_count);
+
+    for (int i = 0; i < strings_count; i++) {
+        string strings_item;
+        getline(cin, strings_item);
+
+        strings[i] = strings_item;
+    }
+
+    int queries_count;
+    cin >> queries_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(strings, queries);
+
+    for (int i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
